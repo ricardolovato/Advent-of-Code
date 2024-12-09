61812fe9151f19bccbeb2dbf2af0d5c223c3ead4
unknown <ricardo.e.lovato@gmail.com>
2024-12-08 19:49:00 -0500

    day 8 complete

--- 2024_day08/2024_day08.py
+++ 2024_day08/2024_day08.py
@@ -3,7 +3,7 @@ import string
 import numpy as np
 
 filename = '2024_day08/input.txt'
-filename = '2024_day08/test_input3.txt'
+# filename = '2024_day08/test_input3.txt'
 
 def print_grid(grid):
     print('     ', end='')
@@ -25,6 +25,15 @@ def visualize_map(lines, nodes, antinodes):
                                        1:'#'}[iT]
     print_grid(ary)
 
+def within_bounds(location):
+    loc_re = np.real(location)
+    loc_im = np.imag(location)
+
+    if loc_re < 0 or loc_re >= lines.shape[0]:
+        return False
+    if loc_im < 0 or loc_im >= lines.shape[1]:
+        return False
+    return True
 
 with open(filename) as f_in:
     lines = np.array([[c for c in line.strip()] for line in f_in.readlines()])
@@ -49,9 +58,39 @@ for node_name, node_locations in nodes.items():
         for node2 in node_locations:
             if node1 == node2:
                 continue
-            d = node1-node2
-            print(f'{node1} -> {node2} = {d}')
-            antinode_location = node1 + d
-            if np.real(antinode_location) < lines.shape[0] and np.imag(antinode_location) < lines.shape[1]:
-                antinodes[node_name].append(node1 + d)
+            distance = node1 - node2
+            # print(f'{node1} -> {node2} = {distance}')
+            antinode_location = node1 + distance
+            if within_bounds(antinode_location) and antinode_location not in antinodes[node_name]:
+                antinodes[node_name].append(antinode_location)
 visualize_map(lines, nodes, antinodes)
+
+# for node_name, node_locations in antinodes.items():
+#     print(f'{node_name}')
+#     for node_location in node_locations:
+#         print(f'\t{node_location}')
+count = len(list(set([node_location for node_name, node_locations in antinodes.items() for node_location in node_locations])))
+print(f'Num antinodes: {count}')
+
+# Part 2
+antinodes = {}
+for node_name, node_locations in nodes.items():
+    antinodes[node_name] = []
+    for node1 in node_locations:
+        for node2 in node_locations:
+            if node1 == node2:
+                continue
+            distance = node2 - node1
+            antinode_location = node1 + distance
+            # print(f'{node1} -> {node2} = {distance}')
+
+            count = 1
+            while within_bounds(antinode_location):
+                if antinode_location not in antinodes[node_name]:
+                    antinodes[node_name].append(antinode_location)
+                antinode_location = (node1 + distance * count)
+                count += 1
+visualize_map(lines, nodes, antinodes)
+
+count = len(list(set([node_location for node_name, node_locations in antinodes.items() for node_location in node_locations])))
+print(f'Num antinodes: {count}')
