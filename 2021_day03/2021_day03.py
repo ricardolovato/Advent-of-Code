9180fb2d2b172b8829053eb241780b16465c80ea
ricardolovato <153131719+ricardolovato@users.noreply.github.com>
2025-11-05 18:23:19 +0000

    2021 day 3 complete

--- 2021_day03/2021_day03.py
+++ 2021_day03/2021_day03.py
@@ -11,7 +11,27 @@ for current_bits in bits.T:
     else:
         gamma_bits.append(0)
 
-b2d = lambda bits, flip: sum([2**pow for pow, bit in enumerate(gamma_bits[::-1]) if bit == flip])
+b2d = lambda bits, flip: sum([2**pow for pow, bit in enumerate(bits[::-1]) if bit == flip])
 
 gamma, epsilon = [b2d(gamma_bits, flip) for flip in [1, 0]]
 print(f'Sum: {gamma * epsilon}')
+
+# Part 2
+ratings = []
+for func in [np.greater_equal, np.less]:
+    test_bits = np.array(bits)
+    for iB in range(test_bits.shape[1]):
+        if len(test_bits) == 1: 
+            break
+
+        current_bits = test_bits.T[iB]
+        mcb = {True:1, False:0}[func(sum(current_bits), test_bits.shape[0]/2)]
+
+        test_bits = np.array([b for b in test_bits if b[iB] == mcb])
+    ratings.append(b2d(test_bits[0],  1))
+
+o2_rating = ratings[0]
+co2_rating = ratings[1]
+print(f'Oxygen generator rating: {o2_rating}')
+print(f'CO2 scrubber rating: {co2_rating}')
+print(f'{o2_rating * co2_rating}')
