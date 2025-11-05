b2001504c25d6265fa664a2a6c3e2accbe510b91
ricardolovato <153131719+ricardolovato@users.noreply.github.com>
2025-11-05 16:43:14 +0000

    rework part 1

--- 2021_day03/2021_day03.py
+++ 2021_day03/2021_day03.py
@@ -4,14 +4,14 @@ filename = '2021_day03/input.txt'
 with open(filename) as f_in:
     bits = np.array([[int(v) for v in line.strip()] for line in f_in.readlines()])
 
-gamma = []
-for col_num in range(len(bits[0])):
-    num_0, num_1 = [np.where(bits.T[col_num] == v)[0].shape[0] for v in [0, 1]]
-    if num_0 > num_1:
-        gamma.append(0)
+gamma_bits = []
+for current_bits in bits.T:
+    if sum(current_bits) > bits.shape[0]/2:
+        gamma_bits.append(1)
     else:
-        gamma.append(1)
+        gamma_bits.append(0)
 
-# Binary addition
-gamma, epsilon = [sum([2**pow for pow, i in enumerate(gamma[::-1]) if i == rate]) for rate in [1, 0]]
-print(gamma * epsilon)
+b2d = lambda bits, flip: sum([2**pow for pow, bit in enumerate(gamma_bits[::-1]) if bit == flip])
+
+gamma, epsilon = [b2d(gamma_bits, flip) for flip in [1, 0]]
+print(f'Sum: {gamma * epsilon}')
