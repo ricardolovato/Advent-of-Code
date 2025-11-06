70e46786354daa43d2864b147fd18e8ec52b5a2b
ricardolovato <153131719+ricardolovato@users.noreply.github.com>
2025-11-06 18:10:51 +0000

    2021 day 05 complete

--- 2021_day05/2021_day05.py
+++ 2021_day05/2021_day05.py
@@ -12,7 +12,6 @@ points = {}
 for x1, y1, x2, y2 in lines:
     start = [x1, y1]
     stop = [x2, y2]    
-
     if x1 == x2:
         # Same row
         for yi in range(min(y1, y2), max(y1, y2) + 1):
@@ -29,4 +28,19 @@ for x1, y1, x2, y2 in lines:
                 points[(xi, y1)] += 1
     else:
         print(f'non-vertical points: ({x1}, {y1}) -> ({x2}, {y2})')
+        # -------- Part 2 ------------------
+        x_step = {True:1, False:-1}[x1 < x2]
+        y_step = {True:1, False:-1}[y1 < y2]
+
+        for i in range(abs(x2 - x1) + 1):
+            xi = x1 + i * x_step
+            yi = y1 + i * y_step
+            print(f'{xi}, {yi}')
+
+            if (xi, yi) not in points:
+                points[(xi, yi)] = 1
+            else:
+                points[(xi, yi)] += 1
+        # -------- Part 2 ------------------
+        
 print(len([k for k, v in points.items() if v > 1]))
