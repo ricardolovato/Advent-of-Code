1c3ce903e4e489dbbb9552a24d3e8ab56b7e978c
ricardolovato <153131719+ricardolovato@users.noreply.github.com>
2025-11-06 20:13:14 +0000

    initial code

---
+++ 2021_day06/2021_day06.py
@@ -0,0 +1,31 @@
+def print_fish(fish):
+    for fish_num, num_fish in fish.items():
+        print(f'{fish_num}:\t{num_fish}')
+    print('-'*12)
+
+filename = '2021_day06/test_input.txt'
+with open(filename) as f_in:
+    numbers = f_in.readlines()
+numbers = [int(v) for v in numbers[0].strip().split(',')]
+
+fish = {i:0 for i in range(9)}
+# Initial state
+for number in numbers:
+    fish[number] += 1
+
+print_fish(fish)
+num_days = 18
+for day in range(num_days + 1):
+
+    # Shuffle them down 
+    next_val = fish[0]
+    for i in range(8):
+        current_val = fish[i]
+        fish[i] = next_val
+        next_val = current_val
+    
+    fish[6] += next_val
+    fish[8] = next_val
+    
+    print_fish(fish)
+
