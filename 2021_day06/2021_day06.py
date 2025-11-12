418a795c2768edc4a63292d268dae1434befc71d
ricardolovato <153131719+ricardolovato@users.noreply.github.com>
2025-11-12 17:35:59 +0000

    2021 day 06 complete

--- 2021_day06/2021_day06.py
+++ 2021_day06/2021_day06.py
@@ -1,9 +1,12 @@
 def print_fish(fish):
-    for fish_num, num_fish in fish.items():
-        print(f'{fish_num}:\t{num_fish}')
-    print('-'*12)
+    for i in range(8, -1, -1):
+        print(f'{i}: ', end = '')
+        for day_num in range(len(fish[0])):            
+            print(f'{fish[i][day_num]} ', end = '')
+        print()
 
 filename = '2021_day06/test_input.txt'
+filename = '2021_day06/input.txt'
 with open(filename) as f_in:
     numbers = f_in.readlines()
 numbers = [int(v) for v in numbers[0].strip().split(',')]
@@ -13,12 +16,11 @@ fish = {i:[0] for i in range(9)}
 for number in numbers:
     fish[number][-1] += 1
 
-# print_fish(fish)
-num_days = 18
-for day in range(num_days + 1):
-    # Shuffle them down 
-    next_val = fish[0][-1]
-    for i in range(8):
+num_days = 256
+for day in range(num_days):
+    # Start at the top and work to 0 
+    next_val = fish[8][-1]
+    for i in range(7, -1, -1):
         current_val = fish[i][-1]
         fish[i].append(next_val)
         next_val = current_val
@@ -26,5 +28,6 @@ for day in range(num_days + 1):
     fish[6][-1] += next_val
     fish[8].append(next_val)
     
-    # print_fish(fish)
-a = 1
+# print_fish(fish)
+print(sum([fish[i][-1] for i in range(0, 9)]))
+
