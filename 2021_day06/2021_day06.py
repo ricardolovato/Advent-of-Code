b30d9c8c1f22f8d13cc7dc8ebe01460bd07ba6eb
unknown <ricardo.e.lovato@gmail.com>
2025-11-11 19:38:19 -0500

    in progress

--- 2021_day06/2021_day06.py
+++ 2021_day06/2021_day06.py
@@ -8,24 +8,23 @@ with open(filename) as f_in:
     numbers = f_in.readlines()
 numbers = [int(v) for v in numbers[0].strip().split(',')]
 
-fish = {i:0 for i in range(9)}
+fish = {i:[0] for i in range(9)}
 # Initial state
 for number in numbers:
-    fish[number] += 1
+    fish[number][-1] += 1
 
-print_fish(fish)
+# print_fish(fish)
 num_days = 18
 for day in range(num_days + 1):
-
     # Shuffle them down 
-    next_val = fish[0]
+    next_val = fish[0][-1]
     for i in range(8):
-        current_val = fish[i]
-        fish[i] = next_val
+        current_val = fish[i][-1]
+        fish[i].append(next_val)
         next_val = current_val
     
-    fish[6] += next_val
-    fish[8] = next_val
+    fish[6][-1] += next_val
+    fish[8].append(next_val)
     
-    print_fish(fish)
-
+    # print_fish(fish)
+a = 1
