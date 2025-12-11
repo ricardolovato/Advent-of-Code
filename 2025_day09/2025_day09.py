dd66c77fc68946c56b8e3b6765a8def4dbbf6a95
ricardolovato <153131719+ricardolovato@users.noreply.github.com>
2025-12-11 18:10:53 +0000

    2025 day 09 complete

--- 2025_day09/2025_day09.py
+++ 2025_day09/2025_day09.py
@@ -5,46 +5,49 @@ def print_grid():
     buff = 4
     max_x, max_y = [max([loc[i] for loc in locations]) for i in range(2)]
     with open('2025_day09/grid.txt', 'w') as f_out:
-        # print(f'\t', end='')
+        print(f'\t', end='')
         f_out.write(f'\t')
         for jy in range(0, max_y + buff):
-            # print(f'{jy} ', end='')
+            print(f'{jy} ', end='')
             f_out.write(f'{jy} ')
-        # print()
+        print()
         f_out.write('\n')
         for ix in range(0, max_x + buff):
-            # print(f'{ix}\t', end='')
+            print(f'{ix}\t', end='')
             f_out.write(f'{ix}\t')
             for jy in range(0, max_y + buff):
-                if ix + 1j * jy in red_tiles:
-                    # print('# ', end='')
-                    f_out.write('# ')
-                elif ix + 1j * jy in green_tiles:
-                    # print('X ', end='')
-                    f_out.write('X ')
-                # if [ix, jy] in locations:
+                # if ix + 1j * jy in red_tiles:
                 #     # print('# ', end='')
                 #     f_out.write('# ')
-                # # elif ix + 1j*jy in all_points:
-                # #     print('@ ', end = '')
-                # elif [ix, jy] in outer_edges:
+                # elif ix + 1j * jy in green_tiles:
                 #     # print('X ', end='')
                 #     f_out.write('X ')
+                if [ix, jy] in locations:
+                    print('# ', end='')
+                    f_out.write('# ')
+                # # elif ix + 1j*jy in all_points:
+                # #     print('@ ', end = '')
+                elif [ix, jy] in outer_edges:
+                    print('X ', end='')
+                    f_out.write('X ')
+                elif ix+1j*jy in exterior_points:
+                    print('O ', end='')
+                    f_out.write('X ')
                 # elif ix + 1j*jy in inner_points:
                 #     print('O ', end = '')
-                elif ix + 1j * jy in fence:
-                    # print('X ', end='')
-                    f_out.write('O ')
+                # elif ix + 1j * jy in fence:
+                #     # print('X ', end='')
+                #     f_out.write('O ')
                 else:
-                    # print('. ', end='')
+                    print('. ', end='')
                     f_out.write('. ')
-            # print()
+            print()
             f_out.write('\n')
 
 
 filename = '2025_day09/test_input.txt'
 # filename = '2025_day09/test_input2.txt'
-# filename = '2025_day09/input.txt'
+filename = '2025_day09/input.txt'
 with open(filename) as f_in:
     lines = f_in.readlines()
 
@@ -66,6 +69,15 @@ print(max_area)
 # Part 2
 locations = [[int(_v) for _v in v.strip().split(',')] for v in lines]
 
+x_loc, y_loc = [[loc[i] for loc in locations] for i in range(2)]
+# Offset by 1 to give a buffer for exterior fill; guarantees we can use 0 as start point
+x_loc, y_loc = [{l:i+1 for i, l in enumerate(sorted(set(loc)))} for loc in [x_loc, y_loc]]
+
+x_rev, y_rev =[{val:key for key, val in loc.items()} for loc in [x_loc, y_loc]]
+
+locations = [[x_loc[loc[0]], y_loc[loc[1]]] for loc in locations]
+
+
 # Outer Edges
 locations = locations + [locations[0]]
 outer_edges = []
@@ -86,6 +98,8 @@ for idx in range(len(locations) - 1):
 all_points = locations + outer_edges
 all_points = set(p[0] + 1j * p[1] for p in all_points)
 
+x_max = int(max([p.real for p in all_points]))
+y_max = int(max([p.imag for p in all_points]))
 
 # Verify that the curve is continuous by walking the edge
 def verify_edges(start):
@@ -104,20 +118,6 @@ def verify_edges(start):
                 stack.append(next_point)
     return visited
 
-#
-# if len(verify_edges(locations[0][0] + 1j * locations[0][1])) != len(all_points):
-#     print('path not continuous')
-
-
-# visited = set()
-# for point in all_points:
-#     num_adjacent = 0
-#     for direction in [-1, 1, -1j, 1j]:
-#         if point + direction in all_points:
-#             num_adjacent += 1
-#     if num_adjacent < 2:
-#         print(f'{point}: {num_adjacent} adjacent')
-
 
 def find_inner_points_recursive(point, visited=None):
     if visited is None:
@@ -132,7 +132,16 @@ def find_inner_points_recursive(point, visited=None):
     return visited
 
 
-def find_inner_points(start):
+def within_bounds(point):
+    if point.real < 0 or point.imag < 0:
+        return False
+    
+    if point.real > x_max + 1 or point.imag > y_max + 1:
+        return False
+    return True
+
+
+def find_points(start, condition):
     visited = set()
     stack = [start]
 
@@ -144,48 +153,24 @@ def find_inner_points(start):
 
         for direction in [1, -1, 1j, -1j]:
             next_point = point + direction
-            if next_point not in visited and next_point not in all_points:
+            # if next_point not in visited and next_point not in all_points:
+            if within_bounds(next_point) and condition(next_point, visited):
                 stack.append(next_point)
     return visited
 
 
-# Find an inner point
-def first_inner_pt(locations, all_points):
-    max_x, max_y = [max([loc[i] for loc in locations]) for i in range(2)]
-    for ix in range(0, max_x):
-        for jy in range(0, max_y):
-            pt = ix + 1j * jy
-            if pt not in all_points and pt - 1j in all_points and pt - 1 in all_points and pt - 1 - 1j in all_points:
-                return pt
-
-#
-# def find_points(start, condition):
-#     visited = set()
-#     stack = [start]
-#
-#     while stack:
-#         point = stack.pop()
-#         if point in visited:
-#             continue
-#         visited.add(point)
-#
-#         for direction in [1, -1, 1j, -1j]:
-#             next_point = point + direction
-#             if condition(next_point, visited):
-#                 stack.append(next_point)
-#     return visited
-#
-#
-# outer_cond = lambda x, visited: x not in visited and x in all_points
-# inner_cond = lambda x, visited: x not in visited and x not in all_points
-
 if len(verify_edges(locations[0][0] + 1j * locations[0][1])) != len(all_points):
     print('path not continuous')
 
-
-start_pt = first_inner_pt(locations, all_points)
-print(f'start location: {start_pt}')
-inner_points = find_inner_points(start_pt)
+exterior_points = find_points(0+0j,
+                              lambda point, visited: point not in visited and point not in all_points)
+inner_points = set()
+for ix in range(0, x_max + 1):
+    for jx in range(0, y_max + 1):
+        current_point = ix + 1j * jx
+    # if not any(ix+1j*jx in points for points in [all_points,exterior_points]):
+        if current_point not in all_points and current_point not in exterior_points:
+            inner_points.add(ix + 1j*jx)
 
 # Inner points and edges, excluding the given corner points
 # green_tiles = set(inner_points)
@@ -193,21 +178,21 @@ inner_points = find_inner_points(start_pt)
 #     green_tiles.add(point[0] + 1j * point[1])
 green_tiles = inner_points | {p[0] + 1j * p[1] for p in outer_edges}
 red_tiles = {p[0] + 1j * p[1] for p in locations}
+all_tiles = green_tiles | red_tiles
 
 
 # Check if the area is continuous
 def valid_location(p1, p2, all_tiles):
     min_x, max_x = [int(func(p1.real, p2.real)) for func in [min, max]]
     min_y, max_y = [int(func(p1.imag, p2.imag)) for func in [min, max]]
-    for ix in range(min_x, max_x):
-        for jy in range(min_y, max_y):
+    for ix in range(min_x, max_x+1):
+        for jy in range(min_y, max_y+1):
             pt = ix + 1j * jy
             if pt not in all_tiles:
                 return False
     return True
 
 
-all_tiles = green_tiles | red_tiles
 max_area = [None, None, 0]
 for p1 in red_tiles:
     for p2 in red_tiles:
@@ -217,10 +202,16 @@ for p1 in red_tiles:
         if not valid_location(p1, p2, all_tiles):
             continue
 
-        rect = (abs(p1.real - p2.real) + 1) * (abs(p1.imag - p2.imag) + 1)
+        # rect = (abs(p1.real - p2.real) + 1) * (abs(p1.imag - p2.imag) + 1)
+        # if rect > max_area[2]:
+        #     max_area = [p1, p2, rect]
+        p1_orig = x_rev[int(p1.real)] + 1j * y_rev[int(p1.imag)]
+        p2_orig = x_rev[int(p2.real)] + 1j * y_rev[int(p2.imag)]
+        rect = (abs(p1_orig.real - p2_orig.real) + 1) * (abs(p1_orig.imag - p2_orig.imag) + 1)
+        # print(f'{p1}, {p2}, {rect}')
         if rect > max_area[2]:
-            max_area = [p1, p2, rect]
+            max_area = [p1_orig, p2_orig, rect]
 
 print(max_area)
 
-print_grid()
+# print_grid()
